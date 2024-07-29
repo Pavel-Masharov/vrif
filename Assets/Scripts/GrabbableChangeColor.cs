65676e78aa97e8265f1ab0a62826e5b5f0fa3bb3
Pavel-Masharov <leonmasharov@gmail.com>
2024-07-29 13:55:11 +0500

    add change color

---
+++ Assets/Scripts/GrabbableChangeColor.cs
@@ -0,0 +1,29 @@
+using BNG;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VR.CustomScene
+{
+    public class GrabbableChangeColor : GrabbableEvents
+    {
+        [SerializeField] private Material _materialOnGrab;
+        private Material _defoltMaterial;
+        private MeshRenderer _meshRenderer;
+        private void Start()
+        {
+            _meshRenderer = GetComponent<MeshRenderer>();
+            _defoltMaterial = _meshRenderer.sharedMaterial;
+        }
+
+        public override void OnGrab(Grabber grabber)
+        {
+            _meshRenderer.sharedMaterial = _materialOnGrab;
+        }
+        public override void OnRelease()
+        {
+            _meshRenderer.sharedMaterial = _defoltMaterial;
+
+        }
+    }
+}
